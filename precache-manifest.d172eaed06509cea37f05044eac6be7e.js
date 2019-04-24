d920409fac9f2ff6cf2747171fcaf3c175ede34b
omf2333 <1923208364@qq.com>
2019-04-25 00:14:30 +0800

    Updates

---
+++ precache-manifest.d172eaed06509cea37f05044eac6be7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "156640ac1bccceb6b920",
+    "url": "/react-movie-list/static/js/runtime~main.8b55392a.js"
+  },
+  {
+    "revision": "6624dca3118513324088",
+    "url": "/react-movie-list/static/js/main.250ed0b3.chunk.js"
+  },
+  {
+    "revision": "5217b3b952717d82efb0",
+    "url": "/react-movie-list/static/js/2.253bda46.chunk.js"
+  },
+  {
+    "revision": "6624dca3118513324088",
+    "url": "/react-movie-list/static/css/main.5314d003.chunk.css"
+  },
+  {
+    "revision": "5217b3b952717d82efb0",
+    "url": "/react-movie-list/static/css/2.1a01c78d.chunk.css"
+  },
+  {
+    "revision": "b6b4451cc09911140f4cdbe15fbdb935",
+    "url": "/react-movie-list/index.html"
+  }
+];
